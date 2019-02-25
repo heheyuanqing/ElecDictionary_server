b5ac4b306a408a00577eca6c4019b788094c4840
heheyuanqing <2570394541@qq.com>
2019-02-25 23:56:41 +0800

    管理员的操作

--- model/amendWord.js
+++ model/amendWord.js
@@ -1,9 +1,15 @@
 const mysql = require('../config/connect_mysql');
 
 let amend = async (val)=>{
-    let _sql = `SELECT * FROM usr WHERE usr_no="${name}"`;
-
-    let infor =  await mysql.query(_sql);
+    let _insertSql = `INSERT INTO words(en,cn,uk,us) values (?,?,?,?)`;
+    let _deleteSql = `DELETE FROM words where en="${val.en}"`;
+    let infor;
+    switch(val.type){
+        case "insert":  infor =  await mysql.query(_insertSql,[val.en,val.cn,val.uk,val.us]);
+        case "delete":  infor =  await mysql.query(_deletetSql);
+        case "insert":  infor =  await mysql.query(_insertSql,[val.en,val.cn,val.uk,val.us]);
+  
+    }
     return infor;
 }
 
